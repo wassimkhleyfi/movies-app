c10063164052d18bc920147512650b5135bbe8ac
Wassim Khleyfi <wassim.khleyfi@gmail.com>
2021-01-29 12:18:46 +0100

    ajout du checkpoint moviesApp Hooks

---
+++ src/components/MovieList.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import MovieCard from "./oneCard/MovieCard";
+
+
+
+
+export default function MovieList({films,newfilms}) {
+  
+    return (
+    <div className="container-fluid contenedor text-center">
+      <div className="work">
+        <h1 className="text-center ">Movies Bibliotheque</h1>
+        <br/>
+        <div className="container text-center">
+          {(newfilms.length) ? newfilms.map((el, i) => <MovieCard key={i} fiche={el} />) :films.map((el, i) => <MovieCard key={i} fiche={el} />)}
+        </div>
+      </div>
+      
+    </div>
+  );
+}
